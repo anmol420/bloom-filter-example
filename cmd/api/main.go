354060cd0d70473569f55290e5541fe6587f28d3
anmol420 <anmolanand2k15@gmail.com>
2025-11-13 03:12:19 +0530

    Added api config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
 
 	"github.com/anmol420/bloom-filter-example/internal/env"
 )
 
 func main() {
 	port := env.GetStringEnv("PORT")
-	fmt.Println(port)
+	cfg := config{
+		port: port,
+	}
+	app := &application{
+		config: cfg,
+	}
+	if err := app.run(app.mount()); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
 }
